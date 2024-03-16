3093f3a278a7f8c9474ff2c40b2f9fdc316a4cf8
Mahendrakumar001 <awspoc1986@gmail.com>
2024-03-16 12:33:22 +0530

    initial commit-2

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "demo" {
     value = aws_instance.demo.public_ip
   
 }
-
+/* vscode configure
+https://www.freecodecamp.org/news/setup-git-on-mac/
 git config --global user.name "Mahendrakumar001"
 $ git config --global user.email "awspoc1986@gmail.com
+
+ssh -T git@github.com
+*/

3a769e5cb17aaa6d56821b8c464127d9f376bd65
Mahendrakumar001 <awspoc1986@gmail.com>
2024-03-16 11:45:27 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+  backend "remote" {
+    organization = "birlasoft"
+    workspaces {
+       name = "terraform-aws-demo27"
+    }
+    
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+  
+}
+
+
+
+
+resource "aws_instance" "demo" {
+    ami = "ami-0d7a109bf30624c99"
+    instance_type = "t2.micro"
+    tags = {
+        "Name" = "Devserver1"
+    }
+}
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "demo" {
+    value = aws_instance.demo.public_ip
+  
+}
